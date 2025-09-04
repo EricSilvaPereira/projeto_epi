f2e081b9a2db49ad682e2fb60df13b64cfcec136
Eric Silva <ericsilva59590@gmail.com>
2025-09-04 11:34:24 -0300

    alteração nas rotas do app

--- projeto_epi/src/routes.jsx
+++ projeto_epi/src/routes.jsx
@@ -5,8 +5,11 @@ const routes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
-        <Route path="employees" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index path="equipments" element={"EQUIPAMENTOS"}></Route>
+          <Route path="employees" element={"FUNCIONARIOS"}></Route>
+          <Route path="deliverys" element={"ENTREGAS"}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
